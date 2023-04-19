13e2c16c1de8d01c165b06d73c96a97c06385a08
DGUBNYA <92747878+DGUBNYA@users.noreply.github.com>
2023-04-19 22:21:34 +0300

    19.04
    19.04

--- Js/swiper.js
+++ Js/swiper.js
@@ -1,14 +1,14 @@
-//  <script>
-//    var swiper = new Swiper(".mySwiper", {
-//       cssMode: true,
-//       navigation: {
-//         nextEl: ".swiper-button-next",
-//         prevEl: ".swiper-button-prev",
-//       },
-//       pagination: {
-//         el: ".swiper-pagination",
-//       },
-//       mousewheel: true,
-//       keyboard: true,
-//     });
-//   </script>
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 2,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
